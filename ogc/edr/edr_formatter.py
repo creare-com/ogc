e3ef50387deacdc7ecacac1827b301518bd67584
Sam Cranford <scranford@creare.com>
2026-02-06 13:34:35 +0000

    Update formatters to use a common base class

--- ogc/edr/edr_formatter.py
+++ ogc/edr/edr_formatter.py
@@ -2,7 +2,40 @@ from pygeoapi.formatter.base import BaseFormatter
 from pygeoapi.util import to_json
 
 
-class GeoTiffFormatter(BaseFormatter):
+class BaseEdrFormatter(BaseFormatter):
+    """Base formatter for EDR data."""
+
+    def __init__(self, formatter_def: dict):
+        """Initialize the formatter.
+
+        Parameters
+        ----------
+        formatter_def : dict
+            The formatter definition.
+        """
+
+        super().__init__(formatter_def)
+        self.mimetype = formatter_def["mimetype"]
+
+    def write(self, options: dict = {}, data: dict | None = None) -> str:
+        """Generate data in the specified format.
+
+        Parameters
+        ----------
+        options : dict, optional
+            Formatting options, by default {}.
+        data : dict | None, optional
+            Dictionary representation of the data, by default None.
+
+        Returns
+        -------
+        str
+            String representation of the data.
+        """
+        return to_json(data, True) if data is not None else ""
+
+
+class GeoTiffFormatter(BaseEdrFormatter):
     """Formatter for GeoTIFF data. Defined for format link information to be populated."""
 
     def __init__(self, formatter_def: dict):
@@ -16,28 +49,10 @@ class GeoTiffFormatter(BaseFormatter):
 
         super().__init__(formatter_def)
         self.f = "geotiff"
-        self.mimetype = formatter_def["mimetype"]
         self.extension = "tiff"
 
-    def write(self, options: dict = {}, data: dict | None = None) -> str:
-        """Generate data in the specified format.
 
-        Parameters
-        ----------
-        options : dict, optional
-            Formatting options, by default {}.
-        data : dict | None, optional
-            Dictionary representation of the data, by default None.
-
-        Returns
-        -------
-        str
-            String representation of the data.
-        """
-        return to_json(data, True) if data is not None else ""
-
-
-class CoverageJsonFormatter(BaseFormatter):
+class CoverageJsonFormatter(BaseEdrFormatter):
     """Formatter for CoverageJSON data. Defined for format link information to be populated."""
 
     def __init__(self, formatter_def: dict):
@@ -51,22 +66,4 @@ class CoverageJsonFormatter(BaseFormatter):
 
         super().__init__(formatter_def)
         self.f = "coveragejson"
-        self.mimetype = formatter_def["mimetype"]
         self.extension = "json"
-
-    def write(self, options: dict = {}, data: dict | None = None) -> str:
-        """Generate data in the specified format.
-
-        Parameters
-        ----------
-        options : dict, optional
-            Formatting options, by default {}.
-        data : dict | None, optional
-            Dictionary representation of the data, by default None.
-
-        Returns
-        -------
-        str
-            String representation of the data.
-        """
-        return to_json(data, True) if data is not None else ""
